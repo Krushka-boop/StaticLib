5cbde9bb012050079d9ee6cf4c39c4876711a07c
LIZA <LIZA@DESKTOP-BP9E8PF>
2024-12-14 22:27:24 +0300

    Добавьте файлы проекта.

---
+++ StaticLib/StaticLib.cpp
@@ -0,0 +1,12 @@
+﻿
+#include "pch.h"
+#include "framework.h"
+#include "lib.h"
+#include <iostream>
+//круг,квадрат,треугольник
+double PloshCircle(double r) { return 3.14 * r * r; }
+double PloshKvadrat(double a) { return a * a; }
+double PloshTrangle(double a, double b) { return 0.5 * a * b; }
+double PerimetrCircle(double r) { return 2 * 3.14 * r; }
+double PerimetrKvadrat(double a) { return 4 * a; }
+double PerimetrTrangle(double a, double b, double c) { return a + b + c; }
---
+++ StaticLib/lib.h
@@ -0,0 +1,7 @@
+#pragma once
+double PloshCircle(double r);
+double PloshKvadrat(double a);
+double PloshTrangle(double a, double b);
+double PerimetrCircle(double r);
+double PerimetrKvadrat(double a);
+double PerimetrTrangle(double a, double b, double c);
